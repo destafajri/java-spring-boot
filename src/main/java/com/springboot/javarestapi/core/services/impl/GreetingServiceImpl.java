4cdfc5811a0dc9bcf7cbc01b7f1a21b29fc44550
destafajri <>
2023-04-02 01:49:42 +0700

    trial timezone

--- src/main/java/com/springboot/javarestapi/core/services/impl/GreetingServiceImpl.java
+++ src/main/java/com/springboot/javarestapi/core/services/impl/GreetingServiceImpl.java
@@ -4,13 +4,20 @@ import com.springboot.javarestapi.core.services.GreetingService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.TimeZone;
+
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
     @Value("${welcome.text}")
     private String welcomeText;
+
+    @Value("${timezone}")
+    private String timezoneLoc;
+
     @Override
     public String sayHello() {
-        return welcomeText;
+        TimeZone timezone = TimeZone.getTimeZone(timezoneLoc);
+        return welcomeText + " our timezone is "+ timezone.getDisplayName();
     }
 }
