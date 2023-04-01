cdf68ca28b7a0decf454d7203701b1acecbbd505
destafajri <>
2023-04-02 02:06:54 +0700

    get value from application properties

--- src/main/java/com/springboot/javarestapi/core/services/impl/GreetingServiceImpl.java
+++ src/main/java/com/springboot/javarestapi/core/services/impl/GreetingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.springboot.javarestapi.core.services.impl;
 
+import com.springboot.javarestapi.config.ApplicationProperties;
 import com.springboot.javarestapi.core.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +13,16 @@ public class GreetingServiceImpl implements GreetingService {
 
     @Value("${welcome.text}")
     private String welcomeText;
+    private ApplicationProperties appProp;
 
-    @Value("${timezone}")
-    private String timezoneLoc;
+    @Autowired
+    public GreetingServiceImpl(ApplicationProperties appProp) {
+        this.appProp = appProp;
+    }
 
     @Override
     public String sayHello() {
-        TimeZone timezone = TimeZone.getTimeZone(timezoneLoc);
+        TimeZone timezone = TimeZone.getTimeZone(appProp.getTimezone());
         return welcomeText + " our timezone is "+ timezone.getDisplayName();
     }
 }
