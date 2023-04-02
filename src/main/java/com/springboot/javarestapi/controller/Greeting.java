e3f2822979defed48f0a1942d1ed3ac41dd970e7
destafajri <>
2023-04-02 23:58:48 +0700

    edit logging

--- src/main/java/com/springboot/javarestapi/controller/Greeting.java
+++ src/main/java/com/springboot/javarestapi/controller/Greeting.java
@@ -3,7 +3,6 @@ package com.springboot.javarestapi.controller;
 import com.springboot.javarestapi.core.services.GreetingService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,7 @@ public class Greeting {
     public String greetings() {
         log.trace("greeting tracing");
         log.debug("greeting debugging");
-        log.info("greeting info running");
+        log.info("greeting request is started running");
         return greetingService.sayHello();
     }
 }
