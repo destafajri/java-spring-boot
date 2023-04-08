f4eafb68fcf4f0fa5d1aff9c8fa50128f0575d08
destafajri <>
2023-04-09 05:26:19 +0700

    validation password length

--- src/main/java/com/springboot/javarestapi/core/domain/dto/AdminCreateRequestDTO.java
+++ src/main/java/com/springboot/javarestapi/core/domain/dto/AdminCreateRequestDTO.java
@@ -2,9 +2,7 @@ package com.springboot.javarestapi.core.domain.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
@@ -20,5 +18,6 @@ public class AdminCreateRequestDTO {
     @Pattern(regexp = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$", message = "must be username format")
     private String username;
     @NotBlank
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters long")
     private String password;
 }
--- src/main/java/com/springboot/javarestapi/core/domain/dto/AuthorCreateRequestDTO.java
+++ src/main/java/com/springboot/javarestapi/core/domain/dto/AuthorCreateRequestDTO.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
@@ -20,5 +21,6 @@ public class AuthorCreateRequestDTO {
     @Pattern(regexp = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$", message = "must be username format")
     private String username;
     @NotBlank
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters long")
     private String password;
 }
