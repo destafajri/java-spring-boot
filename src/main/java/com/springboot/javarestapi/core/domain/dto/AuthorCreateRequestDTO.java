5763d59a7a848571bb30d25ad113ef61c66a2b1c
destafajri <>
2023-04-09 04:46:20 +0700

    validation on request payload

--- src/main/java/com/springboot/javarestapi/core/domain/dto/AuthorCreateRequestDTO.java
+++ src/main/java/com/springboot/javarestapi/core/domain/dto/AuthorCreateRequestDTO.java
@@ -2,7 +2,9 @@ package com.springboot.javarestapi.core.domain.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
@@ -12,8 +14,10 @@ public class AuthorCreateRequestDTO {
     @NotBlank
     private String name;
     @NotBlank
+    @Email(message = "must be email format")
     private String email;
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$", message = "must be username format")
     private String username;
     @NotBlank
     private String password;
