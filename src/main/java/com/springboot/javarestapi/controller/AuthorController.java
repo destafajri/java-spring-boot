6816ed1507fa8b89b2e60749f627e9dd81455823
destafajri <>
2023-04-12 18:49:57 +0700

    custom error for validation

--- src/main/java/com/springboot/javarestapi/controller/AuthorController.java
+++ src/main/java/com/springboot/javarestapi/controller/AuthorController.java
@@ -2,14 +2,19 @@ package com.springboot.javarestapi.controller;
 
 import com.springboot.javarestapi.core.domain.dto.AuthorCreateRequestDTO;
 import com.springboot.javarestapi.core.domain.dto.AuthorListResponse;
+import com.springboot.javarestapi.core.domain.dto.ResponseData;
 import com.springboot.javarestapi.core.services.AuthorService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -21,9 +26,22 @@ public class AuthorController {
     private final AuthorService authorService;
 
     @PostMapping("/create")
-    public ResponseEntity<String> authorCreate(@RequestBody @Valid AuthorCreateRequestDTO dto) {
+    public ResponseEntity<ResponseData<String>> authorCreate(@RequestBody @Valid AuthorCreateRequestDTO dto, Errors errors) {
+        ResponseData responseData = new ResponseData<>();
+
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+
         authorService.createNewAuthor(dto);
-        return ResponseEntity.created(URI.create("/author/create")).body("SUCCESS");
+
+        responseData.setStatus(HttpStatus.CREATED);
+        responseData.setMessage(Collections.singletonList("Success Create New Author"));
+        return ResponseEntity.created(URI.create("/author/create")).body(responseData);
     }
 
     @GetMapping("/list")
---
+++ src/main/java/com/springboot/javarestapi/core/domain/dto/ResponseData.java
@@ -0,0 +1,17 @@
+package com.springboot.javarestapi.core.domain.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Data
+public class ResponseData<T> {
+    private HttpStatus status;
+    private List<String> message = new ArrayList<>();
+    private T data;
+}
