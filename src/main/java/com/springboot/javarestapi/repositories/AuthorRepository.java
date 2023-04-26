8b6e32f50448f3445dbb46831b8cfd166daafc50
destafajri <>
2023-04-26 11:40:27 +0700

    improve pagination

--- src/main/java/com/springboot/javarestapi/repositories/AuthorRepository.java
+++ src/main/java/com/springboot/javarestapi/repositories/AuthorRepository.java
@@ -2,10 +2,13 @@ package com.springboot.javarestapi.repositories;
 
 import com.springboot.javarestapi.core.domain.entities.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Meta;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +16,7 @@ import java.util.UUID;
 @Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, UUID> {
 
+    @Meta
     @Query(value = "SELECT json_build_object(" +
             " 'id', authors.id," +
             " 'user_id', user_id," +
--- src/main/java/com/springboot/javarestapi/repositories/UserRepository.java
+++ src/main/java/com/springboot/javarestapi/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.springboot.javarestapi.repositories;
 
 import com.springboot.javarestapi.core.domain.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 }
